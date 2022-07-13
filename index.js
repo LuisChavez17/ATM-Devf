77debc1e6cfc66c1a057d833e0b7df852f64f35c
Luis Chavez <goldenchavez17@gmail.com>
2022-07-13 04:00:36 -0500

    Se corrige el problema de conteo en Variable balanceActual y se corrigen las cantidades  de balance de cada usuario

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 
 
-let user1 = ["Luis Chavez", "luischavez94@gmail.com", "LL12345", 5000]
-let user2 = ["Anya Forger", "anyaforger22@gmail.com", "AA56789", 3000]
-let user3 = ["Eren Jeager", "erenjeager18@gmail.com", "EE98765", 7500]
+let user1 = ["Luis Chavez", "luischavez94@gmail.com", "LL12345", 500]
+let user2 = ["Anya Forger", "anyaforger22@gmail.com", "AA56789", 300]
+let user3 = ["Eren Jeager", "erenjeager18@gmail.com", "EE98765", 750]
 
 let users = [user1, user2, user3];
 let userValido;
-let balanceActual;
+
 
 function login() {
     let cliente = document.getElementById("email").value;
@@ -22,8 +22,8 @@ function login() {
         login.className = "divloginOculto"
         let divBanca = document.getElementById("banca")
         divBanca.className = "showBanca";
-        document.getElementById("welcome").innerText = "Welcome to the online bank " + userValido[0]; 
-        document.getElementById("balance").innerText = userValido[3];
+        document.getElementById("welcome").innerText = "Welcome to the online bank MMA " + userValido[0]; 
+        balanceActual = parseFloat( document.getElementById("balance").innerText = userValido[3]);
     } else {
         document.getElementById("error").textContent = "error intenta nuevamente"
     }
@@ -32,15 +32,13 @@ function login() {
 }
 }
 
+let balanceEtiqueta = document.getElementById("balance");
+    balanceEtiqueta.innerText = balanceActual;
 
 function depositar() {
-    let balanceEtiqueta = document.getElementById("balance");
-    balanceActual = parseFloat(userValido[3]);
-    balanceEtiqueta.innerText = balanceActual;
-    
     let monto = parseFloat(document.getElementById("depositar").value)
-    if ((monto+balanceActual) > 9990) {
-        mostrarError("Su cuenta no puede contener mas de $9990 pesos")
+    if ((monto+balanceActual) > 990) {
+        mostrarError("Su cuenta no puede contener mas de $990 pesos")
     } else {
         parseFloat( balanceActual += monto);
         balanceEtiqueta.innerText = balanceActual;
@@ -48,13 +46,9 @@ function depositar() {
 }
 
 function retirar() {
-    let balanceEtiqueta = document.getElementById("balance");
-    let balanceActual = parseFloat(userValido[3]);
-    balanceEtiqueta.innerText = balanceActual;
-    
     let monto = parseFloat(document.getElementById("retirar").value)
     if(monto>=balanceActual){
-        mostrarError("No es posible retirar la cantiad revise su cuenta")
+        mostrarError("No es posible retirar la cantiad revise su cuenta y debe dejar una base de almenos 10 pesos")
     } else if ((balanceActual-monto) < 10){
         mostrarError("Su cuenta debe conservar al menos 10 pesos")
     } else {
